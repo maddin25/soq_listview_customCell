f1d305110e8c8a5856e714f7da0183d76605b651
Martin Patz <mailto@martin-patz.de>
2017-04-28 17:24:40 -0500

    first commit

---
+++ ListViewContextMenu/ListViewContextMenuPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+namespace ListViewContextMenu
+{
+	public partial class ListViewContextMenuPage : ContentPage
+	{
+		public ObservableCollection<MyItem> ListData { get; set; }
+
+		public ListViewContextMenuPage()
+		{
+			InitializeComponent();
+			InitializeDataSource();
+			MyListView.ItemsSource = ListData;
+		}
+
+		private void InitializeDataSource()
+		{
+			ListData = new ObservableCollection<MyItem>();
+
+			ListData.Add(new MyItem { Name = "First Item" });
+			ListData.Add(new MyItem { Name = "Second Item" });
+			ListData.Add(new MyItem { Name = "Third Item" });
+		}
+	}
+}
---
+++ ListViewContextMenu/MyItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+namespace ListViewContextMenu
+{
+	public class MyItem
+	{
+		public string Name { get; set; }
+		public Command OnAction { get; set; }
+
+		public MyItem()
+		{
+			OnAction = new Command((obj) => Debug.WriteLine($"Item {obj.ToString()} clicked"));
+		}
+
+		public override string ToString()
+		{
+			return string.Format($"MyItem: Name={Name}");
+		}
+	}
+}
